db452d26b212c2cf7961deb9d6008a4775977844
Fernando <fernandomoraisdev@gmail.com>
2025-05-18 11:26:59 -0300

    atualizacao

--- usuario.php
+++ usuario.php
@@ -1,14 +1,25 @@
 <?php
+$nome = "Fernando Morais";
 $email = 'fernandomorais.ads@gmail.com';
 $senha = '123456';
 
-echo strlen($senha) . PHP_EOL;
+echo mb_strlen($senha) . PHP_EOL;
 
-if(strlen($senha) < 8){
+if(mb_strlen($senha) < 8){
     echo "Senha insegura" . PHP_EOL;
 }
 
 $posicaoDoArroba = strpos($email, '@');
 
-echo substr($email, 0, $posicaoDoArroba) . PHP_EOL;
+$substr = substr($email, 0, $posicaoDoArroba);
+
+echo mb_strtoupper($usuario) . PHP_EOL;
 echo substr($email, $posicaoDoArroba + 1) . PHP_EOL;
+
+list($nome, $sobrenome) = explode(' ', $nome);
+
+echo "Nome: " . $nome . PHP_EOL;
+echo "sobrenome: " . $sobrenome . PHP_EOL;
+
+$csv = "Fernando Morais, 36 anos, fernandomorais.ads@gmail.com";
+var_dump(explode(',' , $csv));
